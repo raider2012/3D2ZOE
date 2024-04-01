4196f949d98ebdc13bc1ad80de5e27d2622674b2
Raghvesh Prasad <8457056+raider2012@users.noreply.github.com>
2024-04-01 12:29:51 -0400

    Added Multithreading
    Also adds elapsed time calculation for the BruteForce

--- Project_BruteForceMultiThreaded.py
+++ Project_BruteForceMultiThreaded.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import concurrent.futures
 import time
 
 # READ CSV FILES
@@ -11,13 +12,13 @@ def read_adjacency_matrix_from_csv(filename):
 
 #FIND ALL TRIPLES
 def find_all_triples(adj_matrix_AB, adj_matrix_BC, adj_matrix_CA):
-    triples = []
+    triples = [];
     for i in range(adj_matrix_AB.shape[0]):
         for j in range(adj_matrix_BC.shape[0]):
             for k in range(adj_matrix_CA.shape[0]):     
-                if (adj_matrix_AB[i][j] == 1 and adj_matrix_BC[j][k] == 1 )  and (adj_matrix_BC[j][k] == 1 and adj_matrix_CA[k][i] == 1) :
+                if (adj_matrix_AB[i][j] == 1 and adj_matrix_BC[j][k] == 1) and (adj_matrix_BC[j][k] == 1 and adj_matrix_CA[k][i] == 1):
                     triples.append((f"A{i+1}", f"B{j+1}", f"C{k+1}"));
-    return triples
+    return triples;
 
 # CREATE TRIPLES MATRIX A (A.x = 1)
 def create_Tuple_matrix(adj_matrix_AB, adj_matrix_BC, adj_matrix_CA):
@@ -26,14 +27,14 @@ def create_Tuple_matrix(adj_matrix_AB, adj_matrix_BC, adj_matrix_CA):
     num_cols = len(triples);
     matrix_A = np.zeros((num_rows, num_cols), dtype=int);
     for i in range(num_cols):
-        currA,currB,currC = triples[i]; 
-        charA,numA = currA[0],int(currA[1:]);
-        charB,numB = currB[0],int(currB[1:]);
-        charC,numC = currC[0],int(currC[1:]);
-        matrix_A[numA-1][i]=1;
-        matrix_A[adj_matrix_AB.shape[0]+numB-1][i]=1;
-        matrix_A[adj_matrix_AB.shape[0] + adj_matrix_BC.shape[0]+numC-1][i]=1;
-    return matrix_A,triples;
+        currA, currB, currC = triples[i];
+        charA, numA = currA[0], int(currA[1:]);
+        charB, numB = currB[0], int(currB[1:]);
+        charC, numC = currC[0], int(currC[1:]);
+        matrix_A[numA-1][i] = 1;
+        matrix_A[adj_matrix_AB.shape[0] + numB-1][i] = 1;
+        matrix_A[adj_matrix_AB.shape[0] + adj_matrix_BC.shape[0] + numC-1][i] = 1;
+    return matrix_A, triples;
 
 
 def bruteForceX(A, b):
@@ -51,24 +52,29 @@ def bruteForceX(A, b):
     return np.zeros((A.shape[1], 1));
 
 def main():
-    adj_matrix_AB = read_adjacency_matrix_from_csv('setAB_WorstCase_Fail.csv');
+    adj_matrix_AB = read_adjacency_matrix_from_csv('setAB.csv');
     print("Matrix AB:");
     print(adj_matrix_AB);
-    adj_matrix_BC = read_adjacency_matrix_from_csv('setBC_WorstCase_Fail.csv');
+    adj_matrix_BC = read_adjacency_matrix_from_csv('setBC.csv');
     print("Matrix BC:");
     print(adj_matrix_BC);
-    adj_matrix_CA = read_adjacency_matrix_from_csv('setCA_WorstCase_Fail.csv');
+    adj_matrix_CA = read_adjacency_matrix_from_csv('setCA.csv');
     print("Matrix CA:");
     print(adj_matrix_CA);
-    matrix_A,triples = create_Tuple_matrix(adj_matrix_AB,adj_matrix_BC,adj_matrix_CA)
+    matrix_A, triples = create_Tuple_matrix(adj_matrix_AB, adj_matrix_BC, adj_matrix_CA);
     print("Matrix A:");
     print(matrix_A);
-    
-    b = np.ones((matrix_A.shape[0],1 ), dtype=int);
+
+    b = np.ones((matrix_A.shape[0], 1), dtype=int);
     start_time = time.time();
-    x = bruteForceX(matrix_A,b);
-    
-    if(np.all(x==0)):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(bruteForceX, matrix_A, b) for _ in range(4)] 
+        for future in concurrent.futures.as_completed(futures):
+            x = future.result()
+            if np.any(x):
+                break
+
+    if np.all(x == 0):
         print("No Solutions found.");
         end_time = time.time();
         print("Total time take to compute x ->",end_time-start_time," secs");
@@ -77,17 +83,16 @@ def main():
     print(x);
     end_time = time.time();
     print("Total time take to compute x ->",end_time-start_time," secs");
-    print("Triples Set T = " , triples);
+    print("Triples Set T = ", triples);
     indexesM = [];
     tripleSoln = [];
     for i in range(len(x)):
-        if (x[i][0]==1):
+        if x[i][0] == 1:
             indexesM.append(i);
-    
+
     for i in range(len(indexesM)):
         tripleSoln.append(triples[indexesM[i]]);
-    print("Distinct Triples Set M = ",tripleSoln);
-    
+    print("Distinct Triples Set M = ", tripleSoln);
 
 if __name__ == "__main__":
     main();
